88ebab80c352629d1794f1ece46cf9482e6b97ea
jin.wang <jin.wang@greedyint.com>
2016-03-08 20:03:45 +0800

    获取代码。

--- Samples/BasicConsole/Program.cs
+++ Samples/BasicConsole/Program.cs
@@ -25,7 +25,6 @@ namespace SuperWebSocket.Samples.BasicConsole
                 return;
             }
 
-            appServer.NewMessageReceived += new SessionHandler<WebSocketSession, string>(appServer_NewMessageReceived);
 
             Console.WriteLine();
 
@@ -39,6 +38,12 @@ namespace SuperWebSocket.Samples.BasicConsole
 
             Console.WriteLine("The server started successfully, press key 'q' to stop it!");
 
+            // WS服务器时间 & 操作
+            appServer.NewSessionConnected += appServer_NewSessionConnected;
+            appServer.NewMessageReceived += appServer_NewMessageReceived;
+            appServer.SessionClosed += appServer_SessionClosed;
+
+            // 循环监听
             while (Console.ReadKey().KeyChar != 'q')
             {
                 Console.WriteLine();
@@ -52,11 +57,26 @@ namespace SuperWebSocket.Samples.BasicConsole
             Console.WriteLine("The server was stopped!");
             Console.ReadKey();
         }
-
+        
+        // When receive message.
         static void appServer_NewMessageReceived(WebSocketSession session, string message)
         {
+            //Receive the message
+            Console.WriteLine("[{0}] Client: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message);
             //Send the received message back
-            session.Send("Server: " + message);
+            session.Send(string.Format("[{0}] Server: I have got your msg: [ {1} ].", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message));
         }
+        // When client connected
+        static void appServer_NewSessionConnected(WebSocketSession session)
+        {
+            session.Send(string.Format("[{0}] Server: Connected! ", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+        }
+        // When server closed
+        static void appServer_SessionClosed(WebSocketSession session, CloseReason reason)
+        {
+            if (reason == CloseReason.ServerShutdown) return;
+            session.Send(string.Format("[{0}] Server: will closed! close reason: {1} .", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), session.Cookies["name"]));
+        }
+
     }
 }
